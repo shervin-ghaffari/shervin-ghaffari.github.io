9da0f436dfa73c55e7bcf021072981530c322a71
shervin-ghaffari <117146044+shervin-ghaffari@users.noreply.github.com>
2023-10-23 09:59:36 +0330

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// JavaScript to handle smooth scrolling to the "About Me" section
-document.getElementById("more-about-button").addEventListener("click", function () {
-    const aboutSection = document.getElementById("about");
-    if (aboutSection) {
-        aboutSection.scrollIntoView({ behavior: "smooth" });
-    }
-});
-
